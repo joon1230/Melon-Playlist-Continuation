742c6a750ac5b6b5829755cf5ca065d37dba6d66
joon1230 <joonamm@naver.com>
2020-12-18 17:16:01 +0900

    kakao_port

--- KakaoArena/Clustering_km.py
+++ KakaoArena/Clustering_km.py
@@ -20,95 +20,6 @@ class Clustering:
                        "tag_title" : { 'size' : 100 , "window" : 40, 'min_count' : 1 , "sg": 0 , "clu": 100}}
 
 
-    def clustering_(self, result, clu=100):
-
-        print("clustering -ing")
-        result_all = result
-
-        # word2vec 하기
-        result_ = {}
-        for i, res in enumerate(result_all.complex_col):
-            result_[i] = [str(re) for re in res]
-
-        w2v_model = Word2Vec(result_.values(), size=100, window=40, min_count=1, sg=0)
-
-        v_complex = np.asarray(w2v_model.wv.index2word)
-        complex_vec = w2v_model.wv.vectors
-
-        col = [f'vector{i}' for i in range(1, complex_vec.shape[1] + 1)]
-        emb_df = pd.DataFrame(complex_vec, columns=col)
-        emb_df['tv_complex'] = v_complex
-
-        # kmeans
-        kmeans = KMeans(n_clusters=clu)
-        kmeans.fit(complex_vec)
-        emb_df['label'] = kmeans.labels_
-
-        emb_df = emb_df[['tv_complex', 'label']]
-
-        return emb_df
-
-    def clustering_song( self, clu=200 ):
-
-        print("clustering -ing")
-
-        ply_song = self.complex_[["id", "songs"]]
-
-        songs_ = {}
-        for i, sgs in enumerate(ply_song.songs):
-            songs_[i] = [str(song) for song in sgs]
-
-        # embedding
-        w2v_model = Word2Vec(songs_.values(), size=100, window=50, min_count=1, sg=0)
-
-        v_songs = np.asarray(w2v_model.wv.index2word)
-        song_vec = w2v_model.wv.vectors
-
-        col = [f'vector{i}' for i in range(1, song_vec.shape[1] + 1)]
-        emb_df = pd.DataFrame(song_vec, columns=col)
-        emb_df['song_id'] = v_songs
-        emb_df = emb_df[['song_id'] + col]
-
-        # kmeans
-        kmeans = KMeans(n_clusters=clu)
-        kmeans.fit(song_vec)
-
-        emb_df['label'] = kmeans.labels_
-        emb_df = emb_df[['song_id', 'label']]
-
-        return emb_df
-
-    def clustering_tag( self, clu=30, by = "tag" ):
-
-        print("clustering -ing")
-
-        id_tag =  self.complex_[["id", by ]].sort_values(by="id")
-
-        # id_tag_df 저장
-        if by == "tag":
-            id_tag.to_pickle('tag_df.pickle')
-
-        result_ = {}
-        for i, res in enumerate(id_tag.tags):
-            result_[i] = [str(re) for re in res]
-
-        w2v_model = Word2Vec(result_.values(), size=100, window=8, min_count=1, sg=0)
-
-        v_complex = np.asarray(w2v_model.wv.index2word)
-        complex_vec = w2v_model.wv.vectors
-
-        col = [f'vector{i}' for i in range(1, complex_vec.shape[1] + 1)]
-        emb_df = pd.DataFrame(complex_vec, columns=col)
-        emb_df['tv_complex'] = v_complex
-
-        # kmeans
-        kmeans = KMeans( n_clusters=clu )
-        kmeans.fit(complex_vec)
-        emb_df['label'] = kmeans.labels_
-
-        emb_df = emb_df[['tv_complex', 'label']]
-        return emb_df
-
     def clustering_album( self , clu = 100):
 
         print("clustering -ing")
@@ -138,7 +49,7 @@ class Clustering:
         return emb_df, pd.DataFrame(list(zip(self.complex_.id, result_.values())), columns=['id', 'ablums'])
 
 
-    def clustering_test( self, df = None, by = 'song'):
+    def clustering( self, df = None, by = 'song'):
         """
         title_singer / tag_gnr_title / tag_title --> 별도의 데이터 프레임 필요 ( 외부변수 받음 )
         song / tag / 외부변수 받지 않음
